b009acc430cb6c91b1e225348c0e6b0c1222e7b1
Daniel <Daniel.clement37@gmail.com>
2024-10-10 14:05:11 -0500

    Fixed deleting columns

--- src/Components/Modals/AddEditBoard.tsx
+++ src/Components/Modals/AddEditBoard.tsx
@@ -1,9 +1,9 @@
 // AddEditBoard.tsx
-import React, { useContext, useState, useEffect } from 'react';
-import { Board, Column } from "../../types/kanbanTypes";
-import { AppContext } from "../../stateManagement/context/AppContext";
+import React, {useContext, useEffect, useState} from 'react';
+import {Board, Column} from "../../types/kanbanTypes";
+import {AppContext} from "../../stateManagement/context/AppContext";
 import AddEditListItem from "../FormComponents/AddEditListItem";
-import { ADD_BOARD, EDIT_BOARD, ADD_COLUMN, EDIT_COLUMN } from "../../stateManagement/actions/actiontypes";
+import {ADD_BOARD, ADD_COLUMN, DELETE_COLUMN, EDIT_BOARD, EDIT_COLUMN} from "../../stateManagement/actions/actiontypes";
 import {generateUniqueId} from "../../Helpers/generateUUID.ts";
 
 interface AddEditBoardProps {
@@ -12,9 +12,9 @@ interface AddEditBoardProps {
     boardId?: string;
 }
 
-const AddEditBoard = ({ closeModal, isEditMode, boardId }: AddEditBoardProps) => {
-    const { state, dispatch } = useContext(AppContext);
-    const { boards, columns } = state;
+const AddEditBoard = ({closeModal, isEditMode, boardId}: AddEditBoardProps) => {
+    const {state, dispatch} = useContext(AppContext);
+    const {boards, columns} = state;
 
     const initialBoardState: Board = isEditMode && boardId && boards[boardId]
         ? boards[boardId]
@@ -25,20 +25,41 @@ const AddEditBoard = ({ closeModal, isEditMode, boardId }: AddEditBoardProps) =>
 
     const [localBoard, setLocalBoard] = useState<Board>(initialBoardState);
     const [columnsForBoard, setColumnsForBoard] = useState<Column[]>([]);
+    const [errors, setErrors] = useState({
+        name: '',
+        columns: [] as string[],
+    });
+    const [deletedColumnIds, setDeletedColumnIds] = useState<string[]>([]);
 
     useEffect(() => {
         if (isEditMode && boardId) {
-            const existingColumns = Object.values(columns).filter((col) => col.boardId === boardId);
+            const existingColumns = Object.values(columns).filter(
+                (col) => col.boardId === boardId
+            );
             setColumnsForBoard(existingColumns);
+            setErrors((prevErrors) => ({
+                ...prevErrors,
+                columns: existingColumns.map(() => ''),
+            }));
         } else {
-            setColumnsForBoard([
-                { id: '', name: '', boardId: '' },
-            ]);
+            const initialColumns = [{id: '', name: '', boardId: ''}];
+            setColumnsForBoard(initialColumns);
+            setErrors((prevErrors) => ({
+                ...prevErrors,
+                columns: initialColumns.map(() => ''),
+            }));
         }
     }, [isEditMode, boardId, columns]);
 
     const handleNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setLocalBoard({ ...localBoard, name: e.target.value });
+        setLocalBoard({...localBoard, name: e.target.value});
+    };
+
+    const handleNameBlur = () => {
+        setErrors((prevErrors) => ({
+            ...prevErrors,
+            name: localBoard.name.trim() ? '' : "Can't be empty",
+        }));
     };
 
     const handleColumnChange = (index: number, value: string) => {
@@ -48,16 +69,63 @@ const AddEditBoard = ({ closeModal, isEditMode, boardId }: AddEditBoardProps) =>
     };
 
     const addColumn = () => {
-        setColumnsForBoard([...columnsForBoard, { id: '', name: '', boardId: '' }]);
+        setColumnsForBoard([...columnsForBoard, {id: '', name: '', boardId: ''}]);
+        setErrors((prevErrors) => ({
+            ...prevErrors,
+            columns: [...prevErrors.columns, ''],
+        }));
     };
 
     const handleColumnRemove = (index: number) => {
+        const columnToRemove = columnsForBoard[index];
+
+        if (columnToRemove.id) {
+            setDeletedColumnIds([...deletedColumnIds, columnToRemove.id]);
+        }
+
         const newColumns = columnsForBoard.filter((_, i) => i !== index);
         setColumnsForBoard(newColumns);
+
+        const newColumnErrors = errors.columns.filter((_, i) => i !== index);
+        setErrors((prevErrors) => ({
+            ...prevErrors,
+            columns: newColumnErrors,
+        }));
+    };
+    const handleColumnBlur = (index: number) => {
+        setErrors((prevErrors) => {
+            const newColumnErrors = [...prevErrors.columns];
+            newColumnErrors[index] = columnsForBoard[index].name.trim() ? '' : "Can't be empty";
+            return {
+                ...prevErrors,
+                columns: newColumnErrors,
+            };
+        });
     };
 
     const handleSubmit = (e: React.FormEvent) => {
         e.preventDefault();
+
+        let hasErrors = false;
+        const newErrors = {name: '', columns: [] as string[]};
+
+        if (!localBoard.name.trim()) {
+            newErrors.name = "Can't be empty";
+            hasErrors = true;
+        }
+
+        newErrors.columns = columnsForBoard.map((column) => {
+            if (!column.name.trim()) {
+                hasErrors = true;
+                return "Can't be empty";
+            }
+            return '';
+        });
+
+        if (hasErrors) {
+            setErrors(newErrors);
+            return;
+        }
 
         if (isEditMode && boardId) {
             // Update board
@@ -86,6 +154,14 @@ const AddEditBoard = ({ closeModal, isEditMode, boardId }: AddEditBoardProps) =>
                         },
                     });
                 }
+            });
+            deletedColumnIds.forEach((columnId) => {
+                dispatch({
+                    type: DELETE_COLUMN,
+                    payload: {
+                        columnId,
+                    },
+                });
             });
         } else {
             // Add new board
@@ -133,14 +209,20 @@ const AddEditBoard = ({ closeModal, isEditMode, boardId }: AddEditBoardProps) =>
                             <label htmlFor="title" className="text-M grey-text">
                                 Name
                             </label>
-                            <input
-                                type="text"
-                                id="name"
-                                className="input-field text-L"
-                                placeholder="e.g. Web Design"
-                                value={localBoard.name}
-                                onChange={handleNameChange}
-                            />
+                            <div className="input-field-container">
+                                <input
+                                    type="text"
+                                    id="name"
+                                    className={`input-field text-L ${errors.name ? 'input-field--error' : ''}`}
+                                    placeholder="e.g. Web Design"
+                                    value={localBoard.name}
+                                    onChange={handleNameChange}
+                                    onBlur={handleNameBlur}
+                                />
+                                {errors.name && (
+                                    <span className="input-error-message text-M">{errors.name}</span>
+                                )}
+                            </div>
                         </div>
                         <div className="list-form-container">
                             <label className="text-M grey-text">Columns</label>
@@ -152,7 +234,9 @@ const AddEditBoard = ({ closeModal, isEditMode, boardId }: AddEditBoardProps) =>
                                         value={column.name}
                                         placeholder="e.g. Todo"
                                         onChange={handleColumnChange}
+                                        onBlur={() => handleColumnBlur(index)}
                                         onRemove={handleColumnRemove}
+                                        error={errors.columns[index]}
                                     />
                                 ))}
                             </div>
